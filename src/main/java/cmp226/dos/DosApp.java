13caacdbf11d827e2479499db5a53e2191f86992
hongyuanChrisLi <hychrisli@users.noreply.github.com>
2017-11-05 08:23:11 -0800

    basic setup

---
+++ src/main/java/cmp226/dos/DosApp.java
@@ -0,0 +1,11 @@
+package cmp226.dos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosApp {
+    public static void main(String[] args) {
+	SpringApplication.run(DosApp.class, args);
+    }
+}
