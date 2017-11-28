324b6ea960c2dad15edd8033df8184096ead761b
Melodyxman <melodywang531@gmail.com>
2017-11-27 20:22:01 -0800

    add Entity Rating

---
+++ src/main/java/cmpe/dos/entity/Rating.java
@@ -0,0 +1,88 @@
+package cmpe.dos.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "Rating")
+public class Rating {
+
+    @Id
+    private Integer id;
+
+    @Column(name = "user_id")
+    private String username;
+
+    @Column(name = "order_id")
+    private Integer order_id;
+
+    @Column(name = "dish_id")
+    private Integer dish_id;
+
+    @Column(name = "score")
+    private short score;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "timestamp", nullable = false)
+    private Date timeStamp;
+
+    @Column(name = "comments")
+    private String comments;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getOrder_id() {
+        return order_id;
+    }
+
+    public void setOrder_id(Integer order_id) {
+        this.order_id = order_id;
+    }
+
+    public Integer getDish_id() {
+        return dish_id;
+    }
+
+    public void setDish_id(Integer dish_id) {
+        this.dish_id = dish_id;
+    }
+
+    public short getScore() {
+        return score;
+    }
+
+    public void setScore(short score) {
+        this.score = score;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+}
